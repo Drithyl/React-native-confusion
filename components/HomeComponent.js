3097a4fdf74a37f0ae38d3af7363d04c909dc798
Drithyl <hugo.bujan@gmail.com>
2019-06-10 12:19:20 -0400

    React-Native-Navigation Part 3 - Home Component and Featured Items

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,9 +1,49 @@
 
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
+import { Card } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
+function RenderItem(props)
+{
+  const item = props.item;
+
+  if (item == null)
+  {
+    return (
+      <View></View>
+    );
+  }
+
+  return (
+    <Card
+      featuredTitle={item.name}
+      featuredSubtitle={item.designation}
+      image={require("./images/uthappizza.png")}
+    >
+      <Text style={{margin: 10}}>
+        {item.description}
+      </Text>
+    </Card>
+  );
+}
 
 class Home extends Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    this.state =
+    {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
+  }
+
   static navigationOptions =
   {
     title: "Home"
@@ -12,7 +52,11 @@ class Home extends Component
   render()
   {
     return (
-      <View><Text>Home Component</Text></View>
+      <ScrollView>
+        <RenderItem item={this.state.dishes.find((dish) => dish.featured)} />
+        <RenderItem item={this.state.promotions.find((promo) => promo.featured)} />
+        <RenderItem item={this.state.leaders.find((leader) => leader.featured)} />
+      </ScrollView>
     );
   }
 }
