34f9661ec17406d070b38950ed77f054eb822698
Drithyl <hugo.bujan@gmail.com>
2019-06-17 13:24:26 -0400

    Adding an Alert to the Delete Favorite

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Alert } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -46,7 +46,22 @@ class Favorites extends Component
           type: "delete",
           onPress: () =>
           {
-            this.props.deleteFavorite(item.id);
+            Alert.alert(
+              "Delete Favorite?",
+              `Are you sure you wish to delete the favorite dish ${item.name}?`,
+              [
+                {
+                  text: "Cancel",
+                  onPress: () => console.log(`${item.name} Not Deleted`),
+                  style: "cancel"
+                },
+                {
+                  text: "Ok",
+                  onPress: () => this.props.deleteFavorite(item.id)
+                }
+              ],
+              { cancelable: false }
+            );
           }
         }
       ];
