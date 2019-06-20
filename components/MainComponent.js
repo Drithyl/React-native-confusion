bdd94b9ac4af62dc6f5ccde398c0e46fb626bfb1
Drithyl <hugo.bujan@gmail.com>
2019-06-20 11:52:08 -0400

    Login Screen and Secure Storage

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import Reservation from "./ReservationComponent";
 import Favorites from "./FavoriteComponent";
+import Login from "./LoginComponent";
 
 const mapStateToProps = (state) =>
 {
@@ -187,6 +188,28 @@ const FavoritesNavigator = createStackNavigator({
   })
 });
 
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login }
+},
+{
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerLeft:
+    <Icon
+      name="menu"
+      size={24}
+      color="white"
+      onPress={() => navigation.toggleDrawer()}
+    />
+  })
+});
+
 const CustomDrawerContentComponent = (props) => (
 
   <ScrollView>
@@ -214,6 +237,24 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+  Login:
+  {
+    screen: LoginNavigator,
+    navigationOptions:
+    {
+      title: "Login",
+      drawerLabel: "Login",
+      drawerIcon: ({ tintColor }) =>
+      (
+        <Icon name="sign-in"
+              type="font-awesome"
+              size={24}
+              color={tintColor}
+        />
+      )
+    }
+  },
+
   Home:
   {
     screen: HomeNavigator,
@@ -322,6 +363,7 @@ const MainNavigator = createDrawerNavigator({
   }
 },
 {
+  initialRouteName: "Home",
   drawerBackgroundColor: "#D1C4E9",
   //defines the layout of the drawer to be what's specified in CustomDrawerContentComponent
   contentComponent: CustomDrawerContentComponent
